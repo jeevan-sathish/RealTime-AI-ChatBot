c1793b2593aeb0bb4f448cdd44ad4ea736381c8a
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-27 23:42:59 +0530

    user login system using class simple programming logic building for backend

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,66 @@
-class app:
-    databse =["jeevan","chintu"]
+class Ecart:
 
-    def __init__(self,name):
-        self.name =name
+    users = [
+        {"name": "jeevan", "password": 1234},
+        {"name": "chintu", "password": 3451}
+    ]
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
 
     def login(self):
-        print("log in succesfull")
-    
-    def dashboard(self):
-        print(f"this si {self.name}")
+        print("Login successful")
+        self.dashboard()
 
-    def validate(self):
-        if self.name in self.databse:
-            self.login()
-            self.dashboard()
-        else:
-            print("log in failed")
+    def dashboard(self):
+        print(f"Welcome {self.name} to Ecart")
+
+    def validation(self):
+        for user in self.users:
+            if user["name"] == self.name and user["password"] == self.password:
+                self.login()
+                return  
+
+      
+        self.users.append({
+            "name": self.name,
+            "password": self.password
+        })
+        print("Account created")
+        data = [ele for ele in self.users if ele["name"] == self.name]
+        print(data)
+        self.login()
+
+
+name = input("Enter your name: ")
+password = int(input("Enter password: "))
+
+k = Ecart(name, password)
+k.validation()
+
+
+
+
+# class app:
+#     databse =["jeevan","chintu"]
+
+#     def __init__(self,name):
+#         self.name =name
+
+#     def login(self):
+#         print("log in succesfull")
+    
+#     def dashboard(self):
+#         print(f"this si {self.name}")
+
+#     def validate(self):
+#         if self.name in self.databse:
+#             self.login()
+#             self.dashboard()
+#         else:
+#             print("log in failed")
         
 
-obj =app("chintu")
-obj.validate()
+# obj =app("chintu")
+# obj.validate()
